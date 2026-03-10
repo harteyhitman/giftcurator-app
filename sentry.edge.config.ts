28e7dcbf78f7b6a27071c8cbcffea6421a69b588
Azeez Oyegoke <azeezoyegoke@Azeezs-MacBook-Air.local>
2026-03-10 09:58:54 -0700

    fixing main

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,6 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "__DSN__",
-  tracesSampleRate: 1.0,
+  dsn: "https://965bca64294854a6c0f2ff199cb7c0d0@o4511020940066816.ingest.us.sentry.io/4511020944130048",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "__DSN__",
-  tracesSampleRate: 1.0,
+  dsn: "https://965bca64294854a6c0f2ff199cb7c0d0@o4511020940066816.ingest.us.sentry.io/4511020944130048",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
